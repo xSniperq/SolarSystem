85c644dbe6a20baef483988f75f55e2c9ea57daf
xSniperq <50496184+xSniperq@users.noreply.github.com>
2022-09-13 21:52:34 +0200

    Add files via upload

--- startstop.js
+++ startstop.js
@@ -1,12 +1,30 @@
 const startstopbutton = document.querySelector(".startstopbutton");
 let differenttext = 0;
 const sliderspeed = document.querySelector(".range");
+let pausedcalenderrefresh;
+// The function is checking for any changes in the calender to update the model
+function PausedCalenderRefresh() {
+  // Code from the function solarintervalstart
+  today = Date.parse(datecalender.value);
+  timecounter = parseInt((today - standardday) / 86400000);
+  // Comparing calender to the css value
+  if (
+    (today - standardday) / 86400000 !=
+    r.style.getPropertyValue("--day-counter")
+  ) {
+    r.style.setProperty("--day-counter", `${timecounter}`);
+    console.log("Model updated");
+  }
+}
+
 function checkdatecalenter() {
   if (differenttext == 0) {
     startstopbutton.innerHTML = "Start";
     differenttext += 1;
     clearInterval(solarinterval);
+    pausedcalenderrefresh = setInterval(PausedCalenderRefresh, 1000);
   } else {
+    clearInterval(pausedcalenderrefresh);
     startstopbutton.innerHTML = "Stop";
     differenttext -= 1;
     if (Date.parse(datecalender.value) < -6000000000000) {
@@ -25,11 +43,10 @@ datecalender.onclick = () => {
   startstopbutton.innerHTML = "Start";
   differenttext = 1;
   clearInterval(solarinterval);
-  r.style.setProperty("--day-counter", `${timecounter}`);
 };
 
 sliderspeed.onclick = () => {
-  intervalspeed =  60 - sliderspeed.value; // Reversing the slider
+  intervalspeed = 60 - sliderspeed.value; // Reversing the slider
   clearInterval(solarinterval);
   startstopbutton.innerHTML = "Start";
   differenttext = 1;
